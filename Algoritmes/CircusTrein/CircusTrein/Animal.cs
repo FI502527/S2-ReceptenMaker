6b3d13aa7e5de3e452c5cf70d076a863263d9898
FI502527 <502527@student.fontys.nl>
2023-09-22 18:53:18 +0200

    Circustrein upload and folder structure

---
+++ Algoritmes/CircusTrein/CircusTrein/Animal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircusTrein
+{
+	public class Animal
+	{
+		private Type Type;
+		private Size Size;
+		private string Name;
+
+		public override string ToString()
+		{
+			string baseString = Type.ToString() + " - " + Size.ToString() + " - " + Name; 
+			return baseString;
+		}
+		public Animal(Type type, Size size, string name)
+		{
+			this.Type = type;
+			this.Size = size;
+			this.Name = name;
+		}
+		public Size GetSize()
+		{
+			return this.Size;
+		}
+		public Type GetType()
+		{
+			return this.Type;
+		}
+		public int GetAnimalPoints()
+		{
+			int animalPoints = 0;
+			switch (GetSize())
+			{
+				case Size.Small:
+					animalPoints = 1;
+					break;
+				case Size.Medium:
+					animalPoints = 3;
+					break;
+				case Size.Large:
+					animalPoints = 5;
+					break;
+			}
+			return animalPoints;
+		}
+	}
+}
---
+++ Algoritmes/CircusTrein/CircusTrein/SortWagons.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircusTrein
+{
+	public class SortWagons
+	{
+		private List<Wagon> wagons = new List<Wagon>();
+		public List<Wagon> GetWagons()
+		{
+			return wagons;
+		}
+		public void StartSort(List<Animal> animals)
+		{
+			int wagonNumber = 1;
+			AddNewWagon(wagonNumber);
+			foreach (Animal animal in animals)
+			{
+				for (int i = 0; i < wagons.Count; i++)
+				{
+					Wagon wagon = wagons[i];
+					bool animalAdded = wagon.AddAnimal(animal);
+					if (!animalAdded)
+					{
+						int added = i + 1;
+						if(wagons.Count == added)
+						{
+							wagonNumber++;
+							AddNewWagon(wagonNumber);
+							wagons[i + 1].AddAnimal(animal);
+							break;
+						}
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+		}
+		public void AddNewWagon(int wagonNumber)
+		{
+			string wagonName = "Wagon" + wagonNumber.ToString();
+			Wagon wagon = new Wagon(wagonName);
+			wagons.Add(wagon);
+		}
+	}
+}
